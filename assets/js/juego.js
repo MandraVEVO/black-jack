b3f60be2f0b74fc0fa1e8908656c5d007d04b598
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2026-02-04 10:16:35 -0600

    creacion de juego black-jack

---
+++ assets/js/juego.js
@@ -0,0 +1,164 @@
+/*
+
+*/
+
+//creacion de deck
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
+
+let puntosJugador = 0;
+let puntosComputadora = 0;
+
+//referencias de html
+const btnPedir = document.querySelector('#btnPedir');
+const btnDetener = document.querySelector('#btnDetener');
+const btnNuevo = document.querySelector('#btnNuevo');
+
+const puntosHtml = document.querySelectorAll('small');
+const divCartasJugador = document.querySelector('#jugador-cartas')
+const divCartasComputadora = document.querySelector('#compu-cartas')
+
+const crearDeck = () => {
+
+    //creacion del deck
+    for(let i = 2; i<=10; i++){
+        for(let tipo of tipos){
+            deck.push(i + tipo);
+        }
+    }
+
+    for(let tipo of tipos){
+        for(let especial of especiales){
+            deck.push(especial + tipo);
+        }
+    }
+
+    // console.log(deck);
+
+    // Algoritmo de Fisher-Yates para mezclar el deck
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+    
+    console.log(deck);
+    return deck;
+}
+
+crearDeck();
+
+//funcion para tomar una carta
+ 
+const pedirCarta = () => {
+
+    if (deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
+    
+    let carta = deck.pop();
+
+    // console.log(carta);
+    return carta;
+}
+
+
+
+
+const valorCarta = (carta) => {
+
+    const valor = carta.substring(0, carta.length -1);
+    return (isNaN(valor)) ?
+            (valor === 'A') ? 11 : 10
+            : valor * 1;
+
+}
+
+//turno computadora
+const turnoComputador = (puntosMinimos)=>{
+    do{
+     const carta = pedirCarta();
+    puntosComputadora = puntosComputadora + valorCarta(carta);
+    puntosHtml[1].innerText = puntosComputadora;
+
+    // <img class="carta" src="assets/cartas/cartas/2C.png" alt=""></img>
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+    divCartasComputadora.append(imgCarta);
+
+    if(puntosMinimos > 21){
+        break;
+    }
+
+
+    } while((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21) );
+
+    setTimeout(() => {
+        if(puntosComputadora > 21){
+        alert('Gano la compu!');
+    }else if(puntosComputadora === puntosMinimos){
+        alert('empate papus!');
+    }else if (puntosComputadora > 21){
+        alert('Gano el cabroncete!');
+    }else {
+        alert('Gano la compu!');
+    }
+    },10);
+}
+
+//eventos 
+
+btnPedir.addEventListener('click', () => {
+
+    const carta = pedirCarta();
+    puntosJugador = puntosJugador + valorCarta(carta);
+    puntosHtml[0].innerText = puntosJugador;
+
+    // <img class="carta" src="assets/cartas/cartas/2C.png" alt=""></img>
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+    divCartasJugador.append(imgCarta);
+    
+    if(puntosJugador > 21){
+        console.warn('Lo siento mucho, perdiste');
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputador(puntosJugador);
+    } else if (puntosJugador === 21){
+        console.warn('21, PAPU!');
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputador(puntosJugador);
+    }
+
+}); //funcion call-back
+
+btnDetener.addEventListener('click', () => {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputador(puntosJugador);
+});
+
+btnNuevo.addEventListener('click', () => {
+    deck = [];
+    deck = crearDeck();
+
+    puntosJugador = 0;
+    puntosComputadora = 0;
+
+    puntosHtml[0].innerText = 0;
+    puntosHtml[1].innerText = 0;
+
+    divCartasComputadora.innerHTML = '';
+    divCartasJugador.innerHTML = '';
+
+    btnPedir.disabled = false;
+    btnDetener.disabled = false;
+
+    console.clear();
+
+
+});
+
